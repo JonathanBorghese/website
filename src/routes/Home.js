4af80e6fec51467a71345e55b4839fcbde70a97e
JonathanBorghese <115730332+JonathanBorghese@users.noreply.github.com>
2024-04-18 16:39:20 -0400

    Added skills list to homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import '../css/Home.css'
+
 function Home() {
+
   return (
     <>
+      <div className="main-div">
       <h1>Home Page</h1>
+
+        <h3>About Me</h3>
+
+        <h3>Skills</h3>
+        <div className="skills-div">
+          { ["C/C++", "Python", "Java", "JavaScript", "SQL", "Git", "Linux", "Artificial Intelligence", "Machine Learning", "Reinforcement Learning", "Computer Vision", "NodeJS", "Angular", "React", "Pandas", "Scikit Learn", "Tensorflow", "OpenCV", "Keras"].map(name => (<div key={name}>{name}</div>)) }
+        </div>
+      </div>
     </>
   );
 }
